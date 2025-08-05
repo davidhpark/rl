1a704ec106c445f9094d4ee7bb6d10d6909c3584
David Park <davidhansulpark@gmail.com>
2025-08-04 21:51:23 -0400

    Fix function signatures

--- squared/squared.h
+++ squared/squared.h
@@ -5,19 +5,19 @@ typedef struct {
   int size;
 
   /* Shared-memory buffers */
-  unsigned char* observations; // length size*size
-  int* actions;
-  float* rewards;
-  unsigned char* terminals;
+  unsigned char *observations; // length size*size
+  int *actions;
+  float *rewards;
+  unsigned char *terminals;
 
   int agent_x, agent_y;
   int target_x, target_y;
   unsigned int rng;
 } Squared;
 
-void c_reset(Squared* env, unsigned int seed);
-void c_step(Squared* env);
-void c_render(const Squared* env);
-void c_close(Squared* env);
+void c_reset(Squared *env);
+void c_step(Squared *env);
+void c_render(Squared *env);
+void c_close(Squared *env);
 
 #endif // SQUARED_H
